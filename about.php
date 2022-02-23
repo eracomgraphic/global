588b75a5137cbc5d5c63d617c3f97410cc1ab0e6
eracomgraphic <100272323+eracomgraphic@users.noreply.github.com>
2022-02-23 17:02:57 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,278 @@
+
+
+    <!-- Start Page Title 
+    ============================================= -->
+    <div class="page-title-area shadow dark bg-fixed text-center text-light" style="background-image: url(<?= $panel_url;?>assets/img/banner/9.jpg);">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 col-md-12">
+                    <h1>About Us</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Page Title -->
+
+    <!-- Start Breadcrumb 
+    ============================================= -->
+    <div class="breadcrumb-area bg-gray text-center">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 col-md-12">
+                    <ul class="breadcrumb">
+                        <li><a href="<?= base_url();?>index"><i class="fas fa-home"></i> Home</a></li>
+                        
+                        <li class="active">About</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Breadcrumb -->
+
+    <!-- Start About Area
+    ============================================= -->
+    <div class="about-area default-padding" style="padding-top:20px! important;padding-bottom:20px! important">
+        <div class="container">
+            <div class="row about-items">
+                <div class="col-md-4 about-info inc-content">
+                    <h2>We're knowledgeable about <strong>making benefits</strong> higher</h2>
+                    <p>
+                        To sure calm much most long me mean. Able rent long in do we. Uncommonly no it announcing melancholy an in. Mirth learn it he given. Secure shy favour length all twenty denote. He felicity no an at packages.
+                    </p>
+                    <!--<a class="btn circle btn-theme effect btn-sm" href="#">Read More</a>-->
+                </div>
+                <div class="col-md-4 thumb">
+                    <img src="<?= $panel_url;?>assets/img/about/1.jpg" alt="Thumb">
+                </div>
+                <div class="col-md-4 management-quote">
+                    <h4>What Management Say</h4>
+                    <div class="author-box thumb-less management-carousel owl-carousel owl-theme">
+                        <div class="item">
+                            <div class="author-quote">
+                                <blockquote>
+                                    Better of always missed we person mr. September smallness northward situation few her certainty something. 
+                                </blockquote>
+                            </div>
+                            <!--<div class="info">
+                                <h4>Ahmed Kamal - <strong>Chairman</strong></h4>
+                                <img src="<?= $panel_url;?>assets/img/signature.png" alt="signature">
+                            </div>-->
+                        </div>
+                        <div class="item">
+                            <div class="author-quote">
+                                <blockquote>
+                                    Better of always missed we person mr. September smallness northward situation few her certainty something. 
+                                </blockquote>
+                            </div>
+                            <!--<div class="info">
+                                <h4>Junayed Ahmed - <strong>Chairman</strong></h4>
+                                <img src="<?= $panel_url;?>assets/img/signature.png" alt="signature">
+                            </div>-->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End About Area -->
+
+    <!-- Start Expertise Area
+    ============================================= -->
+    <div class="expertise-area text-light default-padding bg-dark" style="padding-top:20px! important;padding-bottom:20px! important">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 info">
+                    <h2>We believe, the <span>passion trying & skill</span> can make a top-performing company.</h2>
+                    <p>
+                        Offering confined entrance no. Nay rapturous him see something residence. Highly talked do so vulgar. Her use behaved spirits and natural attempt say feeling. Effects dearest staying now sixteen nor improve. 
+                    </p>
+                    <p>
+                        Effects dearest staying now sixteen nor improve.  An he observe be it covered delight hastily message. Margaret no ladyship endeavor ye to settling
+                    </p>
+                    <!-- Progress Bar Start -->
+                    <div class="skill-items">
+                        <!-- Progress Bar Start -->
+                        <div class="progress-box">
+                            <h5>Strategy & Analysis <span class="pull-right">88%</span></h5>
+                            <div class="progress">
+                                <div class="progress-bar" role="progressbar" data-width="87"></div>
+                            </div>
+                        </div>
+                        <div class="progress-box">
+                            <h5>Eeconomic growth <span class="pull-right">95%</span></h5>
+                            <div class="progress">
+                                <div class="progress-bar" role="progressbar" data-width="96"></div>
+                            </div>
+                        </div>
+                        <div class="progress-box">
+                            <h5>Achieves goals <span class="pull-right">70%</span></h5>
+                            <div class="progress">
+                                <div class="progress-bar" role="progressbar" data-width="52"></div>
+                            </div>
+                        </div>
+                        <!-- End Progressbar -->
+                    </div>
+                    <!-- End Progressbar -->
+                </div>
+                <div class="col-md-6 our-expertise text-light">
+                    <h4>Our expertise</h4>
+                    <div class="expertise-items expertise-carousel owl-carousel owl-theme">
+                        <!--Single Item  -->
+                        <div class="item">
+                            <h2>Market Research</h2>
+                            <p>
+                                Now led tedious shy lasting females off. Dashwood marianne in of entrance be on wondered possible building. Wondered sociable he carriage in speedily margaret. Up devonshire of he thoroughly insensible alteration. An mr settling occasion insisted distance ladyship so. Not attention say frankness intention out dashwoods now curiosity. Stronger ecstatic as no judgment daughter speedily thoughts. Worse downs nor might she court did nay forth these. 
+                            </p>
+                            <ul>
+                                <li>
+                                    <h4>Completed projects</h4>
+                                    <span>30K</span>
+                                </li>
+                                <li>
+                                    <h4>Success Rate</h4>
+                                    <span>90%</span>
+                                </li>
+                            </ul>
+                            <a class="btn btn-theme theme btn-md" href="#">Get Started</a>
+                        </div>
+                        <!-- End Single Item  -->
+                        <!--Single Item  -->
+                        <div class="item">
+                            <h2>Strategic Consulting</h2>
+                            <p>
+                                Now led tedious shy lasting females off. Dashwood marianne in of entrance be on wondered possible building. Wondered sociable he carriage in speedily margaret. Up devonshire of he thoroughly insensible alteration. An mr settling occasion insisted distance ladyship so. Not attention say frankness intention out dashwoods now curiosity. Stronger ecstatic as no judgment daughter speedily thoughts. Worse downs nor might she court did nay forth these. 
+                            </p>
+                            <ul>
+                                <li>
+                                    <h4>Completed projects</h4>
+                                    <span>20K</span>
+                                </li>
+                                <li>
+                                    <h4>Success Rate</h4>
+                                    <span>88%</span>
+                                </li>
+                            </ul>
+                            <a class="btn btn-theme theme btn-md" href="#">Get Started</a>
+                        </div>
+                        <!-- End Single Item  -->
+                        <!--Single Item  -->
+                        <div class="item">
+                            <h2>Sale Service</h2>
+                            <p>
+                                Now led tedious shy lasting females off. Dashwood marianne in of entrance be on wondered possible building. Wondered sociable he carriage in speedily margaret. Up devonshire of he thoroughly insensible alteration. An mr settling occasion insisted distance ladyship so. Not attention say frankness intention out dashwoods now curiosity. Stronger ecstatic as no judgment daughter speedily thoughts. Worse downs nor might she court did nay forth these. 
+                            </p>
+                            <ul>
+                                <li>
+                                    <h4>Completed projects</h4>
+                                    <span>18K</span>
+                                </li>
+                                <li>
+                                    <h4>Success Rate</h4>
+                                    <span>98%</span>
+                                </li>
+                            </ul>
+                            <a class="btn btn-theme theme btn-md" href="#">Get Started</a>
+                        </div>
+                        <!-- End Single Item  -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Expertise Area -->
+
+    
+
+    <!-- Start Our Services
+    ============================================= --
+    <div class="services-area bg-gray carousel-shadow default-padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <div class="site-heading text-center">
+                        <h4>What we do</h4>
+                        <h2>Our Services</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="services-items services-carousel owl-carousel owl-theme">
+                       
+                        <div class="item">
+                            <span>01</span>
+                            <div class="icon">
+                                <div class="shape-box">
+                                    <span class="dot-shape"></span>
+                                    <span class="dot-shape"></span>
+                                    <span class="dot-shape"></span>
+                                </div>
+                                <i class="flaticon-analysis"></i>
+                            </div>
+                            <h4>Business Strategy</h4>
+                            <p>
+                                Minuter believe service arrived civilly add all. Acuteness allowance an at eagerness favourite in extensive exquisite ye. 
+                            </p>
+                            <a href="#">Read More <i class="ti-arrow-right"></i></a>
+                        </div>
+                       
+                        <div class="item">
+                            <span>02</span>
+                            <div class="icon">
+                                <div class="shape-box">
+                                    <span class="dot-shape"></span>
+                                    <span class="dot-shape"></span>
+                                    <span class="dot-shape"></span>
+                                </div>
+                                <i class="flaticon-economy"></i>
+                            </div>
+                            <h4>Resource Allocation</h4>
+                            <p>
+                                Minuter believe service arrived civilly add all. Acuteness allowance an at eagerness favourite in extensive exquisite ye. 
+                            </p>
+                            <a href="#">Read More <i class="ti-arrow-right"></i></a>
+                        </div>
+                        
+                        <div class="item">
+                            <span>03</span>
+                            <div class="icon">
+                                <div class="shape-box">
+                                    <span class="dot-shape"></span>
+                                    <span class="dot-shape"></span>
+                                    <span class="dot-shape"></span>
+                                </div>
+                                <i class="flaticon-money-1"></i>
+                            </div>
+                            <h4>Capital Markets</h4>
+                            <p>
+                                Minuter believe service arrived civilly add all. Acuteness allowance an at eagerness favourite in extensive exquisite ye. 
+                            </p>
+                            <a href="#">Read More <i class="ti-arrow-right"></i></a>
+                        </div>
+                      
+                        <div class="item">
+                            <span>04</span>
+                            <div class="icon">
+                                <div class="shape-box">
+                                    <span class="dot-shape"></span>
+                                    <span class="dot-shape"></span>
+                                    <span class="dot-shape"></span>
+                                </div>
+                                <i class="flaticon-research"></i>
+                            </div>
+                            <h4>Business Strategy</h4>
+                            <p>
+                                Minuter believe service arrived civilly add all. Acuteness allowance an at eagerness favourite in extensive exquisite ye. 
+                            </p>
+                            <a href="#">Read More <i class="ti-arrow-right"></i></a>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Our Services -->
+
+    
